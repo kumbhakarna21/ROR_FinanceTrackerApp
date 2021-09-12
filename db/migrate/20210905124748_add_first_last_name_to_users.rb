a6fdf49a11525f4b94e3e40e4043484b5b152019
Chaitanya Kumbhakarna <chaitanyakumbhakarna@gmail.com>
2021-09-12 19:36:58 +0530

    some layout modification done

--- db/migrate/20210905124748_add_first_last_name_to_users.rb
+++ db/migrate/20210905124748_add_first_last_name_to_users.rb
@@ -2,5 +2,6 @@ class AddFirstLastNameToUsers < ActiveRecord::Migration[6.1]
   def change
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
+    add_column :users, :admin, :boolean, default: false
   end
 end

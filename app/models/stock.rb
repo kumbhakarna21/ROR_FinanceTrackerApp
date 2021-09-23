539964ebaf2450daf6321b56049edfe538764504
Chaitanya Kumbhakarna <chaitanyakumbhakarna@gmail.com>
2021-09-23 23:31:44 +0530

    Img Updated

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,7 +1,6 @@
 class Stock < ApplicationRecord
   has_many :user_stocks
   has_many :users, through: :user_stocks
-
   validates :name, :ticker, presence: true
 
 	def self.new_lookup(ticker_symbol)
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_05_153841) do
+ActiveRecord::Schema.define(version: 2021_09_15_141110) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+ 
 
   create_table "friendships", force: :cascade do |t|
     t.bigint "user_id", null: false
